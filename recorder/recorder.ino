3dbc40a7c9fac773daec29cd4917b97ddd26caa8
Sergio Gasquez Arcos <sergiogasquez@correo.ugr.es>
2016-10-16 19:27:58 +0200

    Add a project with the basics of a voice recorder

---
+++ recorder/recorder.ino
@@ -0,0 +1,20 @@
+int Rec = 11;
+int Play = 13;
+
+void setup()
+{
+  pinMode(Rec, OUTPUT);
+  pinMode(Play, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(Rec, HIGH);
+  delay(10000);
+  digitalWrite(Rec, LOW);
+  delay(5000);
+  digitalWrite(Play, HIGH);
+  delay(100);
+  digitalWrite(Play, LOW);
+  delay(10000);
+}
